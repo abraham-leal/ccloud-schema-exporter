9fd381539d6c805321b20ce1002ebaf28d6088cf
abraham-leal <aleal@confluent.io>
2021-07-07 14:14:41 -0500

    Allow to use with Confluent Cloud API Keys

--- samples/bump_ids.sh
+++ samples/bump_ids.sh
@@ -7,10 +7,10 @@ if (( "$#" < 1 )); then
     echo "Illegal number of parameters."
     echo
     echo "Usage: 
-        $0 <schemaRegistryUrl> [Optional: Beginning Schema ID, Default: 10,000]
+        $0 <schemaRegistryUrl> [Optional: Beginning Schema ID, Default: 10,000] [Optional: Basic Auth Credentials in form of <USERNAME>:<PASSWORD>]
         
     Example: 
-        $0 http://localhost:8081 500
+        $0 http://localhost:8081 500 userAdmin:adminPassword
     "
     exit
 fi
@@ -34,6 +34,17 @@ echo "Starting SR at "$2
 SRBEGINNING=$2
 fi
 
+## Default beginning ID if not set
+if [[ -z $3 ]]
+then 
+echo "No auth provided, proceeding without it"
+AUTH=""
+else
+AUTH=$3
+USERNAME=$(echo $AUTH | sed 's/:.*//')
+echo "Using username: "$USERNAME
+fi
+
 DUMMY_SUBJECT="dummy"
 DUMMY_SUBJECT_2="dummy2"
 SUBJECTMODE_URL=$SRURL'/mode/'$DUMMY_SUBJECT
@@ -46,7 +57,7 @@ SUBJECTSCHEMA_ENDPOINT_2=$SRURL'/subjects/'$DUMMY_SUBJECT_2'/versions'
 CONTENT='Content-Type: application/json'
 
 ## Check we are dealing with a brand new SR
-RESULT_SUBJECTS=$(curl --silent -k $SRURL"/schemas")
+RESULT_SUBJECTS=$(curl -u $AUTH --silent -k $SRURL"/schemas?deleted=true")
 if [ "$RESULT_SUBJECTS" != "[]" ]
 then
 echo "ERROR: Schema Registry is not empty. Aborting."
@@ -54,20 +65,20 @@ exit 1
 fi
 
 ## Set IMPORT on a single subject
-RESULT_SETIMPORT=$(curl --silent -k -X PUT -d "$IMPORT_PAYLOAD" -H "$CONTENT" $SUBJECTMODE_URL)
+RESULT_SETIMPORT=$(curl -u $AUTH --silent -k -X PUT -d "$IMPORT_PAYLOAD" -H "$CONTENT" $SUBJECTMODE_URL)
 
 ## Register dummy schema
-RESULT_REGISTER=$(curl --silent -k -X POST -d "$DUMMYSCHEMA_IMPORT" -H "$CONTENT" $SUBJECTSCHEMA_ENDPOINT)
+RESULT_REGISTER=$(curl -u $AUTH --silent -k -X POST -d "$DUMMYSCHEMA_IMPORT" -H "$CONTENT" $SUBJECTSCHEMA_ENDPOINT)
 
 ## Set READWRITE on a single subject
-RESULT_SETRW=$(curl --silent -k -X PUT -d "$READWRITE_PAYLOAD" -H "$CONTENT" $SUBJECTMODE_URL)
+RESULT_SETRW=$(curl -u $AUTH --silent -k -X PUT -d "$READWRITE_PAYLOAD" -H "$CONTENT" $SUBJECTMODE_URL)
 
 ## Soft delete dummy schema to maintain the beginning ID
-RESULT_DELETE=$(curl --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT"/1")
-HOLD=$(curl --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT"/1?permanent=true")
+RESULT_DELETE=$(curl -u $AUTH --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT"/1")
+HOLD=$(curl -u $AUTH --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT"/1?permanent=true")
 
 ## Register another dummy and confirm ID=SRBEGINNING+1 <- Sanity Check
-OUTPUT=$(curl --silent -k -X POST -d "$DUMMY_SCHEMA" -H "$CONTENT" $SUBJECTSCHEMA_ENDPOINT_2)
+OUTPUT=$(curl -u $AUTH --silent -k -X POST -d "$DUMMY_SCHEMA" -H "$CONTENT" $SUBJECTSCHEMA_ENDPOINT_2)
 SR_REGISTERED=$(echo $OUTPUT | tr -d -c 0-9)
 if (( $SR_REGISTERED >= $SRBEGINNING )); then
   echo "Validation Test Passed."
@@ -77,5 +88,5 @@ else
 fi
 
 ## Cleanup
-HOLD=$(curl --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT_2"/1")
-HOLD=$(curl --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT_2"/1?permanent=true")
+HOLD=$(curl -u $AUTH --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT_2"/1")
+HOLD=$(curl -u $AUTH --silent -k -X DELETE $SUBJECTSCHEMA_ENDPOINT_2"/1?permanent=true")
